99a64da517e005dc0d46a025799b7b900d7b0bdf
thabith <thabith.hm@gmail.com>
2014-11-23 18:16:16 +0530

    MvcLogin

---
+++ MvcLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using MvcLoginApps.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MvcLoginApp.Models;
+
+namespace MvcLoginApps.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Username == "thabit" && model.Password == "hmt")
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else 
+                {
+                    ModelState.AddModelError("", "Invalid Username or Password");
+                }
+            
+            }
+
+            return View();
+        }
+    }
+}
